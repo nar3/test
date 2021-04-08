d1af649824e8df4e669e72afc693e9ce4fb7871a
nar3 <dotmaster13@gmail.com>
2021-04-09 02:26:27 +0430

    part 4 -migrations -add class in "new app" models with title,slug,body,date -admin.site.register(models...) in "new app" admin file -migrations -def __str__ to models class for "new app" -add /objects.all().order_by('date')/ in dict to render return . -fill for loop in "new app" html file

--- django project/market/articles/views.py
+++ django project/market/articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Artic
 
 
 def home(request):
-    return render(request, "home.html")
+    ar = Artic.objects.all().order_by("date")
+    args = {"objects_in_artic_models": ar}
+    return render(request, "articlehome.html", args)
