949cf6eb266702ad9cdb22be4b3ad8ba5734b362
nar3 <dotmaster13@gmail.com>
2021-04-08 20:12:36 +0430

    start market  project

--- django project/market/manage.py
+++ django project/market/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
- 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
